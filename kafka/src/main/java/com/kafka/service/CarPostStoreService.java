0e9e86b031058a1e9dd219aadfac97c8d4601382
LuizLiberatoscioli <luizhat10@gmail.com>
2023-10-09 17:18:40 -0300

    create project structure

---
+++ kafka/src/main/java/com/kafka/service/CarPostStoreService.java
@@ -0,0 +1,17 @@
+package com.kafka.service;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude
+public interface CarPostStoreService {
+
+}
