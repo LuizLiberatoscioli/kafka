d36f71debf339d8542f42c9c7830d8821b13f9b7
LuizLiberatoscioli <luizhat10@gmail.com>
2023-10-20 14:41:13 -0300

    testing kafka with postman.

--- kafka/src/main/java/com/kafka/message/KafkaProducerConfiguration.java
+++ kafka/src/main/java/com/kafka/message/KafkaProducerConfiguration.java
@@ -12,34 +12,33 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.stereotype.Component;
+import org.apache.kafka.common.serialization.StringSerializer;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.kafka.dto.CarPostDto;
 
 @Component
 @Configuration
 public class KafkaProducerConfiguration {
-	
+
 	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServer;
-	
+
 	@Bean
-	public ProducerFactory<String, CarPostDto> userProducerFactory (){
-		
+	public ProducerFactory<String, CarPostDto> userProducerFactory() {
+
 		Map<String, Object> configProps = new HashMap<>();
-		
+
 		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
 		configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
-		
+
 		return new DefaultKafkaProducerFactory<>(configProps);
-		
-		
+
 	}
-	
+
 	@Bean
-	public KafkaTemplate<String, CarPostDto> userKafkaTemplate(){
+	public KafkaTemplate<String, CarPostDto> userKafkaTemplate() {
 		return new KafkaTemplate<>(userProducerFactory());
 	}
 
